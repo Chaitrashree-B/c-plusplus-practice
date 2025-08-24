a5381285bce8a793eb462136071ab00cdfbd0184
chaitrashree-B-tech <chaitrashreeb@hexalytics.com>
2025-08-24 22:40:14 +0530

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+namespace first{
+    int x = 1;
+}
+namespace second{
+    int x = 2;
+}
+int main(){
+    using namespace second;
+    std::cout << first::x;
+   
+
+    return 0;
+}
