49c1f277a2ecb531d4c38141ca623359728df09b
chaitrashree-B-tech <chaitrashreeb@hexalytics.com>
2025-08-29 22:04:19 +0530

    added hypotenese of a triangle

--- mathFunction.cpp
+++ mathFunction.cpp
@@ -3,7 +3,7 @@
 
 
 int main(){
-    double x = 3;
+    double x = 3.99;
     double y = 4;
     double z;
 
@@ -12,6 +12,9 @@ int main(){
   //z= pow(2,4);
 //  z = sqrt(9);
 //z = abs(-3);
+//z = round(x);
+z = ceil(x);
+//z = floor(x);
     std::cout << z;
     return 0;
 }
